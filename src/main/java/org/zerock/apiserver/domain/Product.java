482dabdf793ccb8810ccd44f18fbdf526368bcb4
HwaJin9718 <165185641+HwaJin9718@users.noreply.github.com>
2024-08-09 01:36:39 +0900

    backend - chapter 4.4 all code

---
+++ src/main/java/org/zerock/apiserver/domain/Product.java
@@ -0,0 +1,69 @@
+package org.zerock.apiserver.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "tbl_product")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "imageList")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pno;
+
+    private String pname;
+
+    private int price;
+
+    private String pdesc;
+
+    private boolean delFlag;
+
+    @ElementCollection // 테이블 조인 할 때, @ManyToOne 대신 @ElementCollection 사용
+    @Builder.Default
+    private List<ProductImage> imageList = new ArrayList<>();
+
+    public void changePrice(int price) {
+        this.price = price;
+    }
+
+    public void changeDesc(String desc) {
+        this.pdesc = desc;
+    }
+
+    public void changeName(String name) {
+        this.pname = name;
+    }
+
+    public void addImage(ProductImage image) {
+
+        image.setOrd(imageList.size());
+        imageList.add(image);
+
+    }
+
+    public void addImageString(String fileName) {
+
+        ProductImage productImage = ProductImage.builder()
+                .fileName(fileName)
+                .build();
+
+        addImage(productImage);
+
+    }
+
+    public void clearList() {
+
+        this.imageList.clear();
+
+    }
+
+}
