f92e830411760d45a3592e5c34a38df7b85b29f9
HwaJin9718 <165185641+HwaJin9718@users.noreply.github.com>
2024-08-09 13:03:26 +0900

    backend - chapter 4.5 all code

--- src/test/java/org/zerock/apiserver/repository/ProductRepositoryTests.java
+++ src/test/java/org/zerock/apiserver/repository/ProductRepositoryTests.java
@@ -4,8 +4,11 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.apiserver.domain.Product;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootTest
@@ -26,6 +29,63 @@ public class ProductRepositoryTests {
 
         product.addImageString(UUID.randomUUID() + "_" + "IMAGE1.jpg");
         product.addImageString(UUID.randomUUID() + "_" + "IMAGE2.jpg");
+
+        productRepository.save(product);
+
+    }
+
+    //@Transactional // Transactional 없으면 오류 생성됨
+    @Test
+    public void testRead() {
+
+        Long pno = 1L;
+
+        Optional<Product> result = productRepository.findById(pno);
+
+        Product product = result.orElseThrow();
+
+        log.info(product);
+        log.info(product.getImageList());
+
+    }
+
+    @Test
+    public void testRead2() {
+
+        Long pno = 1L;
+
+        Optional<Product> result = productRepository.selectOne(pno);
+
+        Product product = result.orElseThrow();
+
+        log.info(product);
+        log.info(product.getImageList());
+
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDelete() {
+
+        Long pno = 2L;
+
+        productRepository.updateDelete(pno, true);
+
+    }
+
+    @Test
+    public void testUpdate() {
+
+        Product product = productRepository.selectOne(1L).get();
+
+        product.changePrice(3000);
+
+        product.clearList();
+
+        product.addImageString(UUID.randomUUID() + "_" + "P_IMAGE1.jpg");
+        product.addImageString(UUID.randomUUID() + "_" + "P_IMAGE2.jpg");
+        product.addImageString(UUID.randomUUID() + "_" + "P_IMAGE3.jpg");
 
         productRepository.save(product);
 
